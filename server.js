86bc7316d5964238f11e42f690ef784f2abb3ab5
Abdullah Celen <abdullahcelen009@gmail.com>
2020-07-08 12:53:54 +0100

    updated

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+// const _ = require("lodash");
 const { response } = require("express");
-
+const messages = require("./messages.json");
 const app = express();
 
 app.use(cors(), bodyParser.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-//read all
+//shows all messages
 app.get("/messages", function (req, res) {
   res.send(messages);
 });
 
-//show by Id
+//shows by Id
 app.get("/messages/:id", (req, res) => {
   const messageId = Number(req.params.id);
 
@@ -37,14 +27,30 @@ app.get("/messages/:id", (req, res) => {
     : res.status(404).send("The message is not found");
 });
 
-//post new message
+//posts new message
 app.post("/messages", function (req, res) {
-  console.log(req.body);
-  const newMessage = req.body;
-  messages.push(newMessage);
-  res.json({ success: true });
+  if (req.body.text && req.body.from) {
+    const timeSent = new Date();
+    const newMessage = { ...req.body, timeStamp: timeSent };
+    messages.push(newMessage);
+    res.send({ success: true, newMessage });
+  } else {
+    response.send(400, "Message could not send !");
+  }
 });
 
+//shows last 10 messages
+app.get("/messages/latest", function (req, res) {
+  res.send(_.takeRight(messages, 10));
+});
+
+//deletes a message by Id
+app.delete("/messages/:id", function (req, res) {
+  const messageId = Number(req.params.id);
+
+  messages = messages.filter((item) => item.id !== messageId);
+  response.send({ success: true });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started ${PORT}`);
