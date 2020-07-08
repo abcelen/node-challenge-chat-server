1e77a88ad97f7b5df5b23fd04f232016745fc9aa
Abdullah Celen <abdullahcelen009@gmail.com>
2020-07-09 00:15:30 +0100

    2
    2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-// const _ = require("lodash");
 const { response } = require("express");
-const messages = require("./messages.json");
+let messages = require("./messages.json");
 const app = express();
 
 app.use(cors(), bodyParser.json());
@@ -31,7 +30,13 @@ app.get("/messages/:id", (req, res) => {
 app.post("/messages", function (req, res) {
   if (req.body.text && req.body.from) {
     const timeSent = new Date();
-    const newMessage = { ...req.body, timeStamp: timeSent };
+    // const newMessage = { ...req.body, timeStamp: timeSent, id: };
+    const newMessage = {
+      id: messages.length,
+      from: req.body.from,
+      text: req.body.text,
+      timeStamp: timeSent,
+    };
     messages.push(newMessage);
     res.send({ success: true, newMessage });
   } else {
@@ -40,16 +45,29 @@ app.post("/messages", function (req, res) {
 });
 
 //shows last 10 messages
-app.get("/messages/latest", function (req, res) {
-  res.send(_.takeRight(messages, 10));
+app.get("/messages/latest", (req, res) => {
+  const latestMessages = messages.slice(messages.length - 10, messages.length);
+  res.send(latestMessages);
 });
 
 //deletes a message by Id
 app.delete("/messages/:id", function (req, res) {
-  const messageId = Number(req.params.id);
+  // const messageId = Number(req.params.id);
 
-  messages = messages.filter((item) => item.id !== messageId);
-  response.send({ success: true });
+  // messages = messages.filter((item) => item.id !== messageId);
+  // response.send({ success: true });
+
+  const { id } = req.params;
+  const filteredMessages = messages.filter((message) => {
+    // console.log(typeof message.id, message.id, typeof Number(id), Number(id));
+    return message.id !== Number(id);
+  });
+  if (filteredMessages.length !== messages.length) {
+    messages = filteredMessages;
+    res.send(messages);
+  } else {
+    res.sendStatus(404);
+  }
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
